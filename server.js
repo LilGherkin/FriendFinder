caa72259569da8dc8c48c6b7f8df0dde2f59f703
Mark Hartman <mthartman77@gmail.com>
2019-10-08 13:59:50 -0500

    Fleshed out survery and homepage. Basic things to have the server up and running.

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+//Node modules needed for thing to function. 
 const express = require("express");
 const path = require("path");
-
+//Set up a port that is determined by Heroku, or use port 8080 if not on Heroku. 
 var PORT = process.env.PORT || 8080;
-
+//Enables express per documentation
+var app = express();
 //Set up Express to handle data parsing.
-applicationCache.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+  
